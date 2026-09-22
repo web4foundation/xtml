21e29ba11e4b1269fed78e5c92c78779cee149ea
Rylan Barnes <rylan@barn.es>
2026-09-22 05:19:17 -0500

    First minimal run file

---
+++ demos/MinimalApi/Mouse1.cs
@@ -0,0 +1,36 @@
+#:sdk Microsoft.NET.Sdk.Web
+#:project ../../src/Xtml.WebSocket/Xtml.WebSocket.csproj
+
+using Xtml.WebSocket;
+using Xtml.Dom;
+
+var app = WebApplication.Create(args);
+
+var c = 0;
+var x = 0.0;
+var y = 0.0;
+
+var window = app.MapWindow("/", () => $"""
+    <!doctype html>
+    <html>
+        <body>
+            <h3>Mouse: {x},{y}</h3>
+            <button onclick={OnClick}>
+                Clicks: {c}
+            </button>
+        </body>
+    </html>
+    """);
+
+void OnClick(Event.Mouse e)
+{
+    c++;
+}
+
+window.OnMouseMove = e =>
+{
+    x = e.X;
+    y = e.Y;
+};
+
+app.Run();
